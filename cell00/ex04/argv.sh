d41bed874e06279d3f50c49319553bb7e0e0d68a
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-03-26 09:59:51 +0700

    progress in 0.4

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -4,9 +4,15 @@ if [ "$#" -eq "0" ]
    then
 	echo "No arguments supplied"
 	exit 1
-else
+else if [ "$#" <= "3" ]
 	for var in "$@"
 	do
 		echo $var;
 	done
+else
+	for var in "$@"
+	do
+		if [ "$var" <= "3"]
+		echo $var;
+	done	
 fi
