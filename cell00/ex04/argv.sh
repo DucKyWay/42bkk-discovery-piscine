4671bd6641cfb7acc48a05aea93fb96c3efa2a49
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-03-26 15:24:35 +0700

    finish 0.4

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]
-   then
+then
 	echo "No arguments supplied"
 	exit 1
 elif [ $# -le 3 ]
-	then
+then
 	for var in $@
 	do
 		echo $var;
 	done
 else
-	for var in $@
-	do
-		if [ $var -eq 3 ]
+	for var in $@ 
+	do	
+		if [ $var -eq 3 ] 
 		then
 			break
 		else
