8e75ab85296588535f39e9964f793b3ca1eeaffb
Suphawinee Chatasawapreeda <niji2549@gmail.com>
2023-03-26 21:43:18 +0700

    still on 0.5

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -2,9 +2,17 @@
 
 if [ $# -eq 0 ]
 then
-    echo "No argument Supplied."
-    for var in $@
-    do
-        echo "touch ex"+i 
-    done
+    echo "No arguments supplied"
+elif [ $# -le 3 ]
+then
+	for var in $@
+	do
+		echo $var;
+	done
+else
+	num_vars=3
+	for (( i=1; i<=$num_vars; i++ ))
+	do
+		echo ${!i}
+	done
 fi
