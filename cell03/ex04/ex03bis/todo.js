5e98f242d202972eb6da0578dbd7e48aa75b6ca0
DucKyWay <niji2549@gmail.com>
2023-04-04 16:10:49 +0700

    finish 3.3 and 3,4!!!

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {
+    let new_btn = $("#new_button");
+    let ft_list_div = $("#ft_list");
+
+    let todo_cookies = document.cookie;
+    if (todo_cookies.length !== 0) {
+        todo_cookies = todo_cookies.split(";");
+        todo_cookies.forEach((text) => {
+            text = text.split("=");
+            insert_new_todo(text[0], text[1]);
+        });
+    }
+
+    new_btn.click(() => {
+        let todo_text = prompt("Enter todo list");
+        if (todo_text.trim() === "") {
+            return;
+        }
+
+        let id = new Date().getTime();
+        document.cookie = id + "=" + todo_text + ";";
+        insert_new_todo(id, todo_text);
+    });
+
+    function insert_new_todo(id, text) {
+        let todo = $("<button>")
+            .attr("type", "button")
+            .text(text)
+            .click(() => {
+                let is_confirm = confirm("Do you want to delete me?");
+                if (is_confirm) {
+                    todo.remove();
+                    document.cookie = id + "=; Expires=Thu, 01 Jan 9999 00:00:01 GMT;";
+                }
+            });
+
+        ft_list_div.prepend(todo);
+    }
+});
